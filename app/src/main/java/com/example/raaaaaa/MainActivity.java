2f24f614d8afbfb79464aa33a25923de82dfa2f6
JimenezEvan <evan.ji2184@vusd.us>
2023-03-01 15:00:53 -0800

    dialog added

--- app/src/main/java/com/example/raaaaaa/MainActivity.java
+++ app/src/main/java/com/example/raaaaaa/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.raaaaaa;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton fabEvan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        fabEvan = this.findViewById(R.id.fabEvan);
         int yoMamaWeightTons = Integer.MAX_VALUE;
 
-        boolean isVisible = true; //??????????nerd-emoji????????
+        boolean isVisible = false; //??????????nerd-emoji????????
+        fabEvan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                dialog.setTitle("i think it worked");
+                dialog.setNeutralButton(R.string.fabEvan, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.create().show();
+
+            }
+        });
     }
 
 }
