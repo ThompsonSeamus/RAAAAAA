596a4a3c431494b28bcd7f8fe33e028d44d99419
seamu.th2363 <seamu.th2363@vusd.us>
2023-03-01 15:05:38 -0800

    Seamus update mama update

--- app/src/main/java/com/example/raaaaaa/MainActivity.java
+++ app/src/main/java/com/example/raaaaaa/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.raaaaaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mamaWeight;
+    boolean displayingHerWeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
         int yoMamaWeightTons = Integer.MAX_VALUE;
 
         boolean isVisible = true; //??????????nerd-emoji????????
+        displayingHerWeight = false;
+
+        mamaWeight = findViewById(R.id.yo_mama_weight_button);
+        mamaWeight.setOnClickListener(new View.OnClickListener() {
+            //Boolean displayingHerWeight =
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if(!displayingHerWeight){
+                    mamaWeight.setText(R.string.yo_mamas_weight);
+                    displayingHerWeight = true;
+                }else{
+                    mamaWeight.setText(yoMamaWeightTons + "");
+                    displayingHerWeight = false;
+                }
+            }
+        });
+
+
+
     }
 
 }
